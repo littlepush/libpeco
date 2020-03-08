2b39ba4bb848c311bb4af124f6419a20b81b178c
Push Chen <littlepush+github@gmail.com>
2020-03-08 23:30:58 +0800

    fix gnu check bug

--- PEUtils/peco/utils/mustd.hpp
+++ PEUtils/peco/utils/mustd.hpp
@@ -61,7 +61,7 @@ SOFTWARE.
 #include <csignal>
 #include <stdexcept>
 
-#if defined(__GUNC__) || defined (__GLIBCPP__) || defined (__clang__)
+#if defined(__GLIBC__) || defined (__GLIBCPP__) || defined (__clang__)
 #define __LIBPECO_USE_GNU__
 #endif
 
